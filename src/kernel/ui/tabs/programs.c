f882a4c15b23b2ad4b674607d3c05550015ad50b
Karl Hobley <karlhobley10@gmail.com>
2010-03-09 23:25:10 +0000

    Fixed some errors

--- src/kernel/ui/tabs/programs.c
+++ src/kernel/ui/tabs/programs.c
@@ -13,19 +13,19 @@ struct ui_tab_program_program
 
 unsigned int ui_tab_program_programcount;
 unsigned int ui_tab_program_currentselectedprogram;
-struct ui_tab_program_program[1];
+struct ui_tab_program_program ui_tab_program_programlist[1];
 
 void ui_tab_programs_init()
 {
 //Zero variables
     ui_tab_program_programcount=1;
-    ui_tab_currentselectedprogram=0;
+    ui_tab_program_currentselectedprogram=0;
 
 //Add crashtest
-    ui_tab_program_program[0].name="Crashtest";
-    ui_tab_program_program[0].namelength=sizeof("Crashtest");
-    ui_tab_program_program[0].main=program_crashtest_main;
-    ui_tab_program_program[0].kbhandler=program_crashtest_kbhandler;
+    ui_tab_program_programlist[0].name="Crashtest";
+    ui_tab_program_programlist[0].namelength=sizeof("Crashtest");
+    ui_tab_program_programlist[0].main=program_crashtest_main;
+    ui_tab_program_programlist[0].kbhandler=program_crashtest_kbhandler;
 }
 
 void ui_tab_programs_drawlist()
@@ -48,10 +48,10 @@ void ui_tab_programs_runprogram(unsigned int programid)
     cli_setrect(0);
 
 //Set keyboard handler
-    kb_setkeyboardhandler(ui_tab_program_program[programid].kbhandler);
+    kb_setkeyboardhandler(ui_tab_program_programlist[programid].kbhandler);
 
 //Call main function
-    ui_tab_program_program[programid].main();
+    ui_tab_program_programlist[programid].main();
 
 //Restart UI
     ui_start();
@@ -87,6 +87,6 @@ void ui_tab_programs_kbhandler(unsigned char scancode)
     if(scancode==0x1C)
     {
     //Run program
-        ui_tab_programs_runprogram();
+        ui_tab_programs_runprogram(ui_tab_program_currentselectedprogram);
     }
 }
