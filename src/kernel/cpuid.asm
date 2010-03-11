384b5fead0355ed94c4378df56c6aace71596301
Karl Hobley <karlhobley10@gmail.com>
2010-03-11 18:53:04 +0000

    cpuid now loads in the vendorid

--- src/kernel/cpuid.asm
+++ src/kernel/cpuid.asm
@@ -1,1 +1,16 @@
+;This file gets all the values from CPUID and returns them in a structure
 
+[BITS 32]
+
+extern cpuid_info
+
+global cpuid_run
+cpuid_run:
+    mov eax,0x00
+    cpuid
+    mov [cpuid_info],eax
+    mov [cpuid_info+4],ebx
+    mov [cpuid_info+8],edx
+    mov [cpuid_info+12],ecx
+
+    ret
--- src/kernel/cpuid.c
+++ src/kernel/cpuid.c
@@ -1,1 +1,22 @@
+#include "sys.h"
+#include "libs/memory.h"
 
+struct cpuid_struct cpuid_info;
+
+//Functions from cpuid.asm
+extern void cpuid_run();
+
+
+void cpuid_init()
+{
+//Clear struct
+    memset((unsigned char*)&cpuid_info,0,sizeof(struct cpuid_struct));
+
+//Run CPUID
+    cpuid_run();
+}
+
+struct cpuid_struct* cpuid_getstruct()
+{
+    return &cpuid_info;
+}
