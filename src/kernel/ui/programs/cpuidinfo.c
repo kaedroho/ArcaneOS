0a9906b28e0654df6911f8f29af1d2b37bf94fce
Karl Hobley <karlhobley10@gmail.com>
2010-03-11 22:11:28 +0000

    New program! This program prints out all the data inside the CPUID structure!

---
+++ src/kernel/ui/programs/cpuidinfo.c
@@ -0,0 +1,23 @@
+#include "cli.h"
+#include "ui/ui.h"
+#include "ui/programs.h"
+#include "sys.h"
+
+void program_cpuidinfo_start()
+{
+//Print message
+    cli_puts("Welcome to the ArcaneOS CPUID info program!\n\n");
+    cli_puts("This program shows all the information returned by CPUID\n");
+    cli_puts("Press esc to quit the program\n\n\n");
+
+//Put main CPU ID info
+    struct cpuid_struct* cpuidstruct=cpuid_getstruct();
+    cli_puts("Vendor: ");cli_puts(cpuidstruct->vendor);cli_putch('\n');
+}
+
+void program_cpuidinfo_kbhandler(unsigned char scancode)
+{
+//Esc quits an application
+    if(scancode==0x01)
+        ui_tab_programs_endprogram();
+}
