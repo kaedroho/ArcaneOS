65fa77575511ecc47d76400985cd518771d2b9f4
Karl Hobley <karlhobley10@gmail.com>
2010-03-14 14:20:46 +0000

    Added ticks per seccond

--- src/kernel/timer.c
+++ src/kernel/timer.c
@@ -2,6 +2,7 @@
 #include "cli.h"
 
 unsigned int timer_ticks = 0;
+unsigned int timer_ticksperseccond;
 
 void timer_handler(struct regs *r)
 {
@@ -9,12 +10,15 @@ void timer_handler(struct regs *r)
     timer_ticks++;
 
 //If ticks are a factor of 1000 then 1 seccond has passed
-    if(timer_ticks%1000==0)
+    if(timer_ticks%timer_ticksperseccond==0)
         rtc_tickseccond();
 }
 
 void timer_phase(int hz)
 {
+//Set ticks per seccond
+    timer_ticksperseccond=hz;
+
 //Calculate devisor
     int divisor=1193180/hz;
 
