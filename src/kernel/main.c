db59c07894d034e77b8b44199b3706a02b755213
Karl Hobley <karlhobley10@gmail.com>
2010-03-15 18:14:49 +0000

    Cleaned up.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -43,36 +43,6 @@ int main()
 //Start interrupts
     __asm__ __volatile__ ("sti");
 
-    void* ptr1 = mm_page_kalloc(1);
-    void* ptr2 = mm_page_kalloc(1);
-
-    unsigned int* block_ptr = (unsigned int*)mm_block_alloc(16);
-    block_ptr[0] = 1;
-    block_ptr[1] = 2;
-    block_ptr[2] = 3;
-    block_ptr[3] = 4;
-
-    mm_page_kfree(ptr1,1);
-
-    //*(unsigned int*)ptr1 = 1; // UNCOMMENT THIS LINE TO CAUSE A PAGE FAULT!
-    *(unsigned int*)ptr2 = 1;
-
-    struct cpuid_struct* cpuidstruct=cpuid_getstruct();
-    cli_puts("Vendor: ");cli_puts(cpuidstruct->vendor);cli_putch('\n');
-
-    cli_puts("\n\nCurrent page directory: 0x"); cli_putu32((unsigned int)pg_get_directory(),16);
-    cli_puts("\n\nKernel page directory: 0x"); cli_putu32((unsigned int)pg_kernel_directory,16);
-
-    cli_puts("\n\nAllocated page 1: 0x"); cli_putu32((unsigned int)ptr1,16);
-    cli_puts("\n\nAllocated page 2: 0x"); cli_putu32((unsigned int)ptr2,16);
-
-    cli_puts("\n\nAllocated block: 0x"); cli_putu32((unsigned int)block_ptr,16);
-
-    if (pg_get_enabled())
-        cli_puts("\n\nPaging is enabled!");
-
-    //for (;;);
-
 //Start ui
     ui_start();
 
